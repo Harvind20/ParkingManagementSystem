e7ce87da97b1ee8376db91074423a8345310c200
Sanjeevan Rames <thesilverghost2005@gmail.com>
2026-02-10 16:13:12 +0800

    made parking lot singleton

--- src/coreParkingSystem/ParkingLot.java
+++ src/coreParkingSystem/ParkingLot.java
@@ -1,5 +1,14 @@
 package coreParkingSystem;
+import java.util.*;
 
 public class ParkingLot {
-    
+    private ArrayList<Floor> floors;
+
+    private ParkingLot(){}
+    private static class InstanceHolder{
+        private static final ParkingLot INSTANCE = new ParkingLot();
+    }
+    public static ParkingLot getInstance(){
+        return InstanceHolder.INSTANCE;
+    }
 }
