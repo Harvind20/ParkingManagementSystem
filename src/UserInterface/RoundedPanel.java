2834f83300f5ff55b1a55f9ef1c19345b7254e44
ajjay <50717255+xblxze@users.noreply.github.com>
2026-02-13 03:44:40 +0800

    refactor
    refactored spotselection into separate classes for better structure and reusability

---
+++ src/UserInterface/RoundedPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    int r;
+
+    public RoundedPanel(int r){
+        this.r = r;
+        setOpaque(false);
+    }
+
+    protected void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0,0,getWidth(),getHeight(),r,r);
+        super.paintComponent(g);
+    }
+}
