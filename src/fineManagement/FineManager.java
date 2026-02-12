24b4b41cac82a5dbcfa51db5b57117c97a102fac
Harvind Sethu Pathy <harvindddddd@gmail.com>
2026-02-12 10:16:22 +0800

    Integrated and Tested Backend

--- src/fineManagement/FineManager.java
+++ src/fineManagement/FineManager.java
@@ -2,30 +2,18 @@ package fineManagement;
 
 public class FineManager {
 
-    private FineScheme currentScheme;
+    private static FineScheme currentScheme = new FixedFine(); 
 
     public FineManager() {
-        // Default scheme when system starts
-        currentScheme = new FixedFine();
     }
 
-    // Admin can change fine scheme later
-    public void setFineScheme(FineScheme scheme) {
-        this.currentScheme = scheme;
+    public static void setFineScheme(FineScheme scheme) {
+        currentScheme = scheme;
     }
 
-    // Main method used by ExitSystem
     public double calculateFine(int durationHours) {
-
-        // No fine if parked 24 hours or less
-        if (durationHours <= 24) {
-            return 0;
-        }
-
-        // Calculate only the extra hours beyond 24
+        if (durationHours <= 24) return 0;
         int hoursOverstayed = durationHours - 24;
-
-        // Use the selected scheme to calculate fine
         return currentScheme.calculateFine(hoursOverstayed);
     }
 }
