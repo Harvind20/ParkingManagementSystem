bf5932dbd2d5ee992fdc3fc018e9e50a5c428c97
IsaiahNaden <isaiahnaden22@gmail.com>
2026-02-15 18:08:14 +0800

    adding vip logic

--- src/EntryModule/Vehicle.java
+++ src/EntryModule/Vehicle.java
@@ -5,14 +5,24 @@ public abstract class Vehicle {
     protected String vehicleType;
     protected long entryTime;
     protected String ticketId;
+    protected boolean isVip;
 
     public Vehicle(String licensePlate) {
         this.licensePlate = licensePlate;
         this.entryTime = System.currentTimeMillis();
+        this.isVip = false;
     }
 
     public String getLicensePlate() { return licensePlate; }
     public String getVehicleType() { return vehicleType; }
     public long getEntryTime() { return entryTime; }
     public void setTicketId(String tId) { this.ticketId = tId; }
+
+    public void setVip(boolean vip) {
+        this.isVip = vip;
+    }
+
+    public boolean isVip() {
+        return isVip;
+    }
 }
